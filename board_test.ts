5baf1b90e4f66ffe2427534eafafcd4fe178b465
Benjamin Gerresheim <benjaminmikagerresheim@gmail.com>
2026-06-05 17:15:42 +0200

    syncing current changes from laptop to pc

--- board_test.ts
+++ board_test.ts
@@ -22,3 +22,16 @@ Deno.test("Zahl kann umgeklapt werden.", () => {
     //Assert
     assertEquals(board.getAvailableNumbers(), [1, 2, 4, 5, 6, 7, 8, 9])
 })
+
+Deno.test("Falsche Zahl flippen", () => {
+
+    //Arrange
+    const board = new Board();
+
+    board.flipNumber([3]);
+    board.flipNumber([3]);
+
+    //Assert
+    assertEquals(board.getAvailableNumbers(), [1, 2, 4, 5, 6, 7, 8, 9])
+
+})
