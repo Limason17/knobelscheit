6dddea1290b9329283a18edc81c468655b6ba94b
Benjamin Gerresheim <benjaminmikagerresheim@gmail.com>
2026-06-05 14:44:27 +0200

    Numbers flippen implemented

--- board.ts
+++ board.ts
@@ -1,7 +1,22 @@
 export class Board {
-    private availableNumbers: number[] = [];
+
+    private availableNumbers: number[] = [1,2,3,4,5,6,7,8,9];
+    private flippedNumbers: number[] = [];
 
     getAvailableNumbers(): number[] {
-        return [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        return this.availableNumbers;
+    }
+
+
+    flipNumber(numbers: number[]): void{
+        
+        for(let i = 0; i < numbers.length; i++){
+            for(let j = 0; j < this.availableNumbers.length; j++){
+                if(numbers[i] == this.availableNumbers[j]){
+                    this.flippedNumbers.push(numbers[i])
+                    this.availableNumbers.splice(j, 1)
+                }
+            }
+        }
     }
 }
--- board_test.ts
+++ board_test.ts
@@ -11,3 +11,14 @@ Deno.test("Sind alle Zahlen noch verfügbar", () => {
     //Assert
     assertEquals(availableNumbers, [1, 2, 3, 4, 5, 6, 7, 8, 9]);
 })
+
+Deno.test("Zahl kann umgeklapt werden.", () => {
+    //Arrange
+    const board = new Board();
+
+    //Act
+    board.flipNumber([3]);
+
+    //Assert
+    assertEquals(board.getAvailableNumbers(), [1, 2, 4, 5, 6, 7, 8, 9])
+})
