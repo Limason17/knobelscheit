d00f5afb0db7fd67a9239b218fc9802ba8ff7669
Benjamin Gerresheim <benjaminmikagerresheim@gmail.com>
2026-06-05 14:07:33 +0200

    Summe von zwei Würfel Werten Tests und Code implementation

--- dice.ts
+++ dice.ts
@@ -1,8 +1,12 @@
 export function rollDie(): number{
     const random = Math.random();
-    return Math.floor((random * 6) + 1)
+    return Math.floor((random * 6) + 1);
 }
 
 export function rollDice(): number[] {
-    return [rollDie(), rollDie()]
+    return [rollDie(), rollDie()];
+}
+
+export function sumDice(dice : number[]): number{
+    return dice[0] + dice[1];
 }
--- dice_test.ts
+++ dice_test.ts
@@ -1,5 +1,5 @@
 import {assert} from "@std/assert";
-import { rollDie, rollDice} from "./dice.ts";
+import { rollDie, rollDice, sumDice} from "./dice.ts";
 
 Deno.test("Prüft ob gewürfelte Zahl zwischen 1-6 ist.", () => {
     
@@ -16,4 +16,14 @@ Deno.test("Zwei Würfel würfeln", () => {
     const result = rollDice();
 
     assert(result.length == 2)
+})
+
+Deno.test("Summe von 2 Würfeln", () => {
+    //Arrange
+    const dice = [3,4];
+
+    //Act
+    const result = sumDice(dice);
+
+    assert(result == 7)
 })
